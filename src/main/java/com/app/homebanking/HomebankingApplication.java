176555cf34b3b3dd1aebdbea6da0ce2fe81078fa
Agustin Soria <soriapreacher@gmail.com>
2024-11-26 20:44:15 -0300

    step 1

--- src/main/java/com/app/homebanking/HomebankingApplication.java
+++ src/main/java/com/app/homebanking/HomebankingApplication.java
@@ -1,7 +1,11 @@
 package com.app.homebanking;
 
+import com.app.homebanking.models.Client;
+import com.app.homebanking.repositories.ClientRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HomebankingApplication {
@@ -9,5 +13,13 @@ public class HomebankingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HomebankingApplication.class, args);
 	}
+	@Bean
+	public CommandLineRunner initData(ClientRepository clientRepository){
+		return (args -> {
+			Client client1 = new Client(1, "Melba", "Morel", "melba@mindhub.com");
+			clientRepository.save(client1);
 
+			clientRepository.save(new Client(2, "Ines", "Risotto", "inessirotto@gmail.com"));
+		});
+	}
 }
---
+++ src/main/java/com/app/homebanking/models/Client.java
@@ -0,0 +1,61 @@
+package com.app.homebanking.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Client {
+    @Id
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public Client() {
+    }
+
+    public Client(int id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "clientID=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
